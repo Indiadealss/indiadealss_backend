e2f2ccac722bc7da016403b5f95a52c5349390fb
Indiadealss <indiadealss1@gmail.com>
2025-10-01 18:18:51 +0530

    back upload

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use("/api/auth", authRoutes);
-app.use("/property", propertyRoutes);
+app.use("/api/property", propertyRoutes);
 
 //app.listen(process.env.PORT, () => console.log(`Server running on :${process.env.PORT}`));
 
