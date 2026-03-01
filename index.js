0ee9d7a279f9c53178d0603c0cef14751109065c
Indiadealss <indiadealss1@gmail.com>
2026-03-01 17:52:03 +0530

    add index.html

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ connectDB();
   // header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 const app = express();
 // middlewares
-app.use(express.json({ limit: '200mb' }));
+app.use(express.json({ limit: '900mb' }));
 app.use(express.urlencoded({ limit: '200mb', extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
